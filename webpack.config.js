208ff7103bedf0f11e6a38a98421eed4bc6daf14
Sergey Avanesyan <Sergey_Avanesyan@epam.com>
2017-08-03 12:15:47 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry : [
+		'webpack-hot-middleware/client',
+		'./src/index'
+	],
+	output : {
+		path : path.join(__dirname, 'dist'),
+		filename : 'bundle.js',
+		publicPath : '/static/'
+	},
+	devtool : 'cheap-module-eval-source-map',
+	module : {
+		rules : [
+			{
+				test : /\.js$/,
+				exclude : /node_modules/,
+				use : {
+					loader : 'babel-loader',
+					options : {
+						presets : ["es2015", "stage-0", "react"],
+						plugins : ['transform-runtime']
+					}
+				}
+			}
+		]
+	},
+	plugins : [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+	]
+};
