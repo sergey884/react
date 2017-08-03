a15169abcbe008d66be1b7e5246de2bd79b812ad
Sergey Avanesyan <Sergey_Avanesyan@epam.com>
2017-08-03 18:27:44 +0300

    Create Async react/redux app.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Page from '../components/Page';
+import * as actions from '../actions/actions';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount(){
+		console.log("componentDidMount");;
+		this.props.actions.getItems();
+	}
 	render() {
-		return (
-			<div>
-				Hello from App.js.
-			</div>
-		);
+
+		const {page : { items } } = this.props;
+		return (<div>
+					<Page items={items} />
+				</div>);
+		}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		page : state.page
+	};
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		actions : bindActionCreators(actions, dispatch)
 	}
 }
 
-export default App;
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
